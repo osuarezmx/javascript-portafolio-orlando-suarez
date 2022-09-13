34f663dfa3e590089825150f6a18be9141318011
Orlando Suarez <corxosz@gmail.com>
2022-09-12 20:19:52 -0500

    creacion de recuadros de servicios con js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,197 @@
+//Bienvenida
+let nombrePers = prompt("Ingresa tu nombre");
+
+
+function inicio() {
+    let bienvenida = `Bienvenido ${nombrePers}`;
+    alert(bienvenida);
+}
+
+
+//Variables
+let total=0, precioServicio=0;
+let miCompra = [], totalPagar, posicion = 0, compra="Servicios a cotizar\n";
+let fin = `Gracias por contactarnos ${nombrePers}, nosotros nos pondremos en contacto contigo para más detalles.`
+
+// Servicios
+const servicios = [
+    {nombre: "Desarrollo web", precio:16000},
+    {nombre: "Branding", precio:10000},
+    {nombre: "Marketing", precio:18000},
+];
+
+//Servicios agrupados
+/* const servicios = [
+    {nombre: "diseño y desarrollo web", precio:16000},
+    {nombre: "ui/ux", precio:10000},
+    {nombre: "tiendas digitales", precio:18000},
+    {nombre: "logo", precio:6500},
+    {nombre: "naming", precio:4500},
+    {nombre: "guia de identidad", precio:8000},
+    {nombre: "packaging", precio:12500},
+    {nombre: "materiales impresos", precio:4200},
+    {nombre: "estrategia de marketing", precio:22800},
+    {nombre:"redes sociales", precio:6200},
+    {nombre:"publicidad digital", precio:5200},
+]; */
+
+
+const serviciosWeb = [
+    {nombre: "diseño y desarrollo web", precio:16000},
+    {nombre: "ui/ux", precio:10000},
+    {nombre: "tiendas digitales(e-commerce)", precio:18000},
+];
+const serviciosBrand = [
+    {nombre: "logo", precio:6500},
+    {nombre: "naming", precio:4500},
+    {nombre: "guia de identidad", precio:8000},
+    {nombre: "packaging", precio:12500},
+];
+const serviciosMkt = [
+    {nombre: "materiales impresos", precio:4200},
+    {nombre: "estrategia de marketing", precio:22800},
+    {nombre:"redes sociales", precio:6200},
+    {nombre:"publicidad digital", precio:5200},
+];
+
+
+let serviciosGrid = document.querySelector(".servicios-grid");
+let servicioContenido = document.querySelector(".servicio-contenido");
+
+for (const servicio of servicios) {
+    let servicioContenido = document.querySelector(".servicio-contenido");
+    servicioContenido.innerHTML = `<h2 class="servicio-titulo heading">${servicio.nombre}</h2>`;
+
+    serviciosGrid.append(servicioContenido);
+
+
+    //Lista de cada servicio
+    let servicioLista = document.createElement("ul");
+    servicioContenido.append(servicioLista);
+    servicioLista.className = "servicio-lista";
+
+    if(servicio.nombre === "Desarrollo web"){
+        for (const item of serviciosWeb) {
+            let servWeb = document.createElement("li");
+            servWeb.innerHTML = `${item.nombre} | `;
+            servWeb.className = "elemento-lista texto";
+    
+            servicioLista.append(servWeb);
+        }
+    }else if(servicio.nombre === "Branding"){
+        for (const item of serviciosBrand) {
+            let servBrand = document.createElement("li");
+            servBrand.innerHTML = `${item.nombre} | `;
+            servBrand.className = "elemento-lista texto";
+    
+            servicioLista.append(servBrand);
+        }
+    }else if(servicio.nombre === "Marketing"){
+        for (const item of serviciosMkt) {
+            let servMkt = document.createElement("li");
+            servMkt.innerHTML = `${item.nombre} | `;
+            servMkt.className = "elemento-lista texto";
+    
+            servicioLista.append(servMkt);
+        }
+    }
+    
+
+    //Botones
+    let btnServDiv = document.createElement("div");
+    servicioContenido.append(btnServDiv);
+    btnServDiv.className = "servicio-botones";
+
+    let btnServVer = document.createElement("a");
+    btnServVer.innerHTML = "Ver más";
+    btnServVer.className = "enlace servicio-btn";
+
+    btnServDiv.append(btnServVer);
+
+    let btnServCotizar = document.createElement("a");
+    btnServCotizar.innerHTML = "Cotizar";
+    btnServCotizar.className = "enlace enlace-btn servicio-btn";
+
+    btnServDiv.append(btnServCotizar);
+
+}
+
+
+
+
+/* 
+let servicioLista = document.querySelector()
+for(const servicioWeb of serviciosWeb) {
+    let servicioListaElmnt = document.createElement("li");
+    servicioListaElmnt.innerHTML = `<h2>${servicio.nombre}</h2>`
+    servicioLista.append
+} */
+
+//Funcion para seleccionar serviciosa cotizar
+function cotizar() {
+    
+    let catalogo = servicios.map(item => item.nombre);
+
+    const agregarCarrito = (item) => {
+        miCompra.push(item);
+        console.log(miCompra);
+        return(miCompra);
+    }
+
+    const mostrarCompra = () => {
+        for(let i=0; i<miCompra.length; i++){
+        compra = compra + `${miCompra[i].nombre}, Costo: $${miCompra[i].precio}\n`;}
+        console.log(miCompra);
+        alert(compra);
+        alert(`El total de los servicios es $${total}`);
+    }
+
+    const sumarPrecios = (item) => {
+        precioServicio = parseInt(item.precio);
+        total += precioServicio;
+        return(total);
+    }
+
+    function ingresarServ() {
+        let encontrado=0
+        let seleccion = prompt(`${nombrePers}\n¿Qué servicio desea cotizar?\n${catalogo.join("\n")}`);
+        seleccion = seleccion.toLowerCase();
+        
+
+        for(const item of servicios) {
+            if(seleccion===(item.nombre).toLowerCase()){
+                let mensaje = `Servicio elegido: ${item.nombre}, Precio: $${item.precio}`;
+                alert(mensaje);
+                encontrado = 1;
+                agregarCarrito(item);
+                sumarPrecios(item);
+                return(item);
+            }
+        }
+        if(encontrado === 0)
+        alert("No encontramos el servicio");
+    }
+    
+    ingresarServ();
+
+    let nuevoServ = confirm(`¿Deseas agregar otro servicio?`);
+    while(nuevoServ === true) {
+        ingresarServ();
+        nuevoServ = confirm(`¿Deseas agregar otro servicio?`);
+    }
+    console.log(miCompra)
+    
+    mostrarCompra();
+
+    alert(fin);
+}
+
+
+
+
+
+
+//Primera entrega
 /* let nombre = prompt("Ingresa tu nombre");
 
 
